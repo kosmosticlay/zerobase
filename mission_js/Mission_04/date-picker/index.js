ef4142f8498823b268f8661ab98364fd9a945f3d
kosmosticlay <kosmosticlay@gmail.com>
2024-01-19 17:34:05 +0900

    Render dates for each month

--- mission_js/Mission_04/date-picker/index.js
+++ mission_js/Mission_04/date-picker/index.js
@@ -1,5 +1,6 @@
 const $calendarContainer = document.querySelector(".calendar");
-const $arrowBackward = document.querySelector(".arrow-backward");
+const $datesGrid = document.querySelector(".dates-grid");
+
 let $headerYear = document.querySelector(".nav__header__year");
 let $headerMonth = document.querySelector(".nav__header__month");
 
@@ -47,11 +48,82 @@ function getCurrentObj(direction) {
 
   if (direction === "backward") {
     currentObj.setMonth(currentObj.getMonth() - 1);
-    console.log(currentObj);
   } else if (direction === "forward") {
     currentObj.setMonth(currentObj.getMonth() + 1);
   }
   return currentObj;
+}
+
+// 윤년 구하기
+function checkLunarYear(year) {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// 각 월에 해당하는 달력 업데이트
+function updateCalendarGrid(dateObj) {
+  $datesGrid.innerHTML = "";
+  const monthDayList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (dateObj.getMonth() === 1) {
+    if (checkLunarYear(dateObj.getFullYear())) monthDayList[1] = 29;
+  }
+
+  const firstDayObj = new Date(dateObj.getFullYear(), dateObj.getMonth(), 1);
+
+  const firstDay = firstDayObj.getDay();
+
+  // 각 월의 1일을 기준으로 날짜 출력
+  let tempArr = [];
+  //지난달 i=3까지
+  let preMonthObj = new Date(currentObj);
+  preMonthObj.setMonth(preMonthObj.getMonth() - 1);
+  let preMonthLastDate = monthDayList[preMonthObj.getMonth()];
+
+  for (let i = 0; i < firstDay; i++) {
+    const $tempLi = document.createElement("li");
+    $tempLi.textContent = preMonthLastDate;
+    $tempLi.classList.add("not-this-month");
+
+    if (i === 0) {
+      $datesGrid.appendChild($tempLi);
+      tempArr.push(preMonthLastDate);
+    } else {
+      $datesGrid.insertBefore($tempLi, $datesGrid.firstChild);
+      tempArr.unshift(preMonthLastDate);
+    }
+    preMonthLastDate--;
+  }
+  // 이번달
+  for (let i = 0; i < monthDayList[dateObj.getMonth()]; i++) {
+    const $tempLi = document.createElement("li");
+    $tempLi.textContent = i + 1;
+
+    $datesGrid.appendChild($tempLi);
+
+    tempArr.push(i + 1);
+  }
+
+  // 다음달
+  let totalDaysNeeded = 42;
+  let remainDays = totalDaysNeeded - tempArr.length;
+
+  for (let i = 0; i < remainDays; i++) {
+    const $tempLi = document.createElement("li");
+    $tempLi.textContent = i + 1;
+    $tempLi.classList.add("not-this-month");
+
+    $datesGrid.appendChild($tempLi);
+
+    tempArr.push(i + 1);
+  }
+  console.log(tempArr);
 }
 
 export const updateNavHeader = (direction) => {
@@ -62,6 +134,7 @@ export const updateNavHeader = (direction) => {
     currentObj = getCurrentObj(direction);
     getNavHeader(currentObj);
   }
+  updateCalendarGrid(currentObj);
 };
 
 // 화면에 캘린더를 출력
