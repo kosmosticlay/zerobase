53e09c9e837643ed2a71c0aad0732cf35643ef1e
kosmosticlay <kosmosticlay@gmail.com>
2024-01-24 10:23:01 +0900

    Change HTML elements to be dynamically created

--- mission_js/Mission_02/template/AnalogClock.js
+++ mission_js/Mission_02/template/AnalogClock.js
@@ -1,25 +1,50 @@
-const $container = document.querySelector(".analog-clock");
+const AnalogClock = () => {
+  const $containers = document.querySelectorAll(".analog-clock");
+  $containers.forEach((container) => {
+    const $fragment = document.createDocumentFragment();
 
-const AnalogClock = ($container) => {
-  const $hours = $container.querySelector(".hour");
-  const $minutes = $container.querySelector(".minute");
-  const $seconds = $container.querySelector(".second");
-  const handleClock = () => {
-    const time = new Date();
-    let hours = time.getHours();
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
+    // 시, 분, 초 구조 생성
+    const $hour = document.createElement("div");
+    $hour.className = "hand hour";
+    $fragment.appendChild($hour);
 
-    if (hours >= 12) {
-      hours -= 12;
+    const $minute = document.createElement("div");
+    $minute.className = "hand minute";
+    $fragment.appendChild($minute);
+
+    const $second = document.createElement("div");
+    $second.className = "hand second";
+    $fragment.appendChild($second);
+
+    // 분 구분선 구조 생성
+    for (let i = 1; i <= 12; i++) {
+      const $line = document.createElement("div");
+      $line.className = `time time${i}`;
+      $line.textContent = "|";
+      $fragment.appendChild($line);
     }
-    console.log(hours, minutes, seconds);
-    $hours.style.setProperty("--deg", hours * 30 + minutes * 0.5);
-    $minutes.style.setProperty("--deg", minutes * 6);
-    $seconds.style.setProperty("--deg", seconds * 6);
-  };
 
-  setInterval(handleClock, 1000);
+    container.appendChild($fragment);
+
+    // 시계의 시침, 분침, 초침을 각각 저장
+    const handleClock = () => {
+      const time = new Date();
+      let hours = time.getHours();
+      const minutes = time.getMinutes();
+      const seconds = time.getSeconds();
+
+      if (hours >= 12) {
+        hours -= 12;
+      }
+
+      $hour.style.setProperty("--deg", hours * 30 + minutes * 0.5);
+      $minute.style.setProperty("--deg", minutes * 6);
+      $second.style.setProperty("--deg", seconds * 6);
+    };
+
+    handleClock();
+    setInterval(handleClock, 1000);
+  });
 };
 
 export default AnalogClock;
