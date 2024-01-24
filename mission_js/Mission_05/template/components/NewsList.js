48dcf97ec1347f44e02afedfaff5fe40bce37087
kosmosticlay <kosmosticlay@gmail.com>
2024-01-24 13:22:49 +0900

    Add comments detailing key features of functions

--- mission_js/Mission_05/template/components/NewsList.js
+++ mission_js/Mission_05/template/components/NewsList.js
@@ -1,10 +1,9 @@
-// do something!
+// 현재 선택 중인 카테고리
 let globeCategory;
 
 const $root = document.querySelector("#root");
 
 const API_KEY = "adcae596004446108cfadfae2a2fa775";
-let currentPage = 1;
 
 function CreateDOM(element, className) {
   const $el = document.createElement(element);
@@ -14,7 +13,9 @@ function CreateDOM(element, className) {
   return $el;
 }
 
-// obj = {cateogry : "all"}
+// 현재 로드된 페이지 수
+let currentPage = 1;
+
 async function fetchData(obj) {
   const fetchUrl = `https://newsapi.org/v2/top-headlines?country=kr&category=${
     obj.category === "all" ? "" : obj.category
@@ -73,7 +74,7 @@ function CreateNewsItem(dataObj) {
 }
 
 async function renderNewsList(obj) {
-  // 기존 fetchData 함수 호출 수정: 페이지 번호를 파라미터로 전달
+  // fetchData 함수 호출 수정: 페이지 번호를 파라미터로 전달
   const newsData = await fetchData(obj, currentPage);
   const newsArray = newsData.articles;
 
@@ -88,11 +89,13 @@ async function renderNewsList(obj) {
   updateObserver();
 }
 
+// 현재 선택중인 카테고리를 기준으로 데이터 추가로 로드하기
 async function loadMore() {
-  currentPage += 1; // 페이지 번호 증가
-  await renderNewsList(globeCategory); // 카테고리 수정 필요
+  currentPage += 1;
+  await renderNewsList(globeCategory);
 }
 
+// Intersection Observer이 감지시 실행 할 콜백함수
 const observerCallback = (entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -101,27 +104,28 @@ const observerCallback = (entries, observer) => {
   });
 };
 
+// Intersection Observer 생성
 const observer = new IntersectionObserver(observerCallback, {
   root: null,
   rootMargin: "0px",
   threshold: 1.0,
 });
 
+// 페이지 가장 마지막 요소에 옵저버 등록
 function updateObserver() {
   const lastElement = document.querySelector(".news-list").lastElementChild;
   if (lastElement) {
-    console.log("working!");
     observer.observe(lastElement);
   }
 }
 
+// NewsList 구조 생성 -> 페이지 로드 후 렌더링(renderNewsList)
 export default function NewsList(obj) {
   globeCategory = obj;
 
   let $newsListContainer = document.querySelector(".news-list-container");
   let $newsList = document.querySelector(".news-list");
 
-  // 만약 news-list-container가 존재하지 않으면 새로 생성합니다.
   if (!$newsListContainer) {
     $newsListContainer = CreateDOM("div", "news-list-container");
     $newsList = CreateDOM("article", "news-list");
